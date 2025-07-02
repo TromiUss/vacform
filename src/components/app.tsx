a8dcb84666e8059b17ba8a0d1ee235391afcd8b1
TromiUss <121058447+TromiUss@users.noreply.github.com>
2025-07-02 23:18:47 +0300

    react-router-dom

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,99 +1,101 @@
-import { useState } from "react";
+import { JSX, useState } from "react";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { useVacancyViewModel } from "../vac-form-viewmodel";
 import VacForm from "../view/vac-form-view";
 import VacAddView from "../view/vac-add-view";
 import VacEditView from "../view/vac-edit-view";
 import { Vacancy } from "@model/vacancy";
 
+const App = (): JSX.Element => {
+  const { vacancies, handleAddVacancy, handleUpdateVacancy } =
+    useVacancyViewModel();
+  const [mode, setMode] = useState("view");
+  const [selectedVacancy, setSelectedVacancy] = useState<Vacancy | null>(null);
+  const [activeButton, setActiveButton] = useState<string | null>("view");
 
-
-const App = () => {
-    const { vacancies, handleAddVacancy, handleUpdateVacancy } =
-      useVacancyViewModel();
-    const [mode, setMode] = useState("view");
-    const [selectedVacancy, setSelectedVacancy] = useState<Vacancy | null>(null);
-    const [activeButton, setActiveButton] = useState<string | null>("view");
-  
-    const handleViewClick = () => {
-      setMode("view");
-      setActiveButton("view");
-    };
-  
-    const handleAddClick = () => {
-      setMode("add");
-      setActiveButton("add");
-    };
-  
-    const handleEditClick = (vacancy: Vacancy) => {
-      setSelectedVacancy(vacancy);
-      setMode("edit");
-      setActiveButton(null);
-    };
-  
-    return (
-      <section>
-        <header className="page-header">
-          <div className="page-header__container">
-            <div className="vac-link">
-              <button
-                onClick={handleViewClick}
-                className={`list-button ${
-                  activeButton === "view" ? "selected-button" : ""
-                }`}
-              >
-                Все заявки
-              </button>
-              <button
-                onClick={handleAddClick}
-                className={`new-button ${
-                  activeButton === "add" ? "selected-button" : ""
-                }`}
-              >
-                Создание заявки
-              </button>
-            </div>
-          </div>
-        </header>
-  
-        <main className="page-main">
-          <div className="page-body__container">
-            {mode === "view" && (
-              <VacForm vacancies={vacancies} onEditClick={handleEditClick} />
-            )}
-            {mode === "add" && (
-              <VacAddView
-                initialValues={vacancies}
-                onSubmit={handleAddVacancy}
-                onSuccess={() => setMode("view")}
-              />
-            )}
-            {mode === "edit" && (
-              <VacEditView
-                initialValues={{
-                  VacName: selectedVacancy?.VacName || "",
-                  Ot: selectedVacancy?.Ot || "",
-                  salaryFrom: selectedVacancy?.salaryFrom || "",
-                  salary: selectedVacancy?.salary || "",
-                  salaryTo: selectedVacancy?.salaryTo || "",
-                  address: selectedVacancy?.address || "",
-                  underground: selectedVacancy?.underground || "",
-                  date: selectedVacancy?.date || "",
-                  PlanDate: selectedVacancy?.PlanDate || "",
-                  gender: selectedVacancy?.gender || "",
-                  experience: selectedVacancy?.experience || "",
-                  gr: selectedVacancy?.gr || "",
-                  employment_type: selectedVacancy?.employment_type || "",
-                }}
-                onUpdateVacancy={handleUpdateVacancy}
-                onCancel={() => setMode("view")}
-                setMode={setMode}
-              />
-            )}
-          </div>
-        </main>
-      </section>
-    );
+  const handleViewClick = () => {
+    setMode("view");
+    setActiveButton("view");
   };
 
-  export default App;
-  
+  const handleAddClick = () => {
+    setMode("add");
+    setActiveButton("add");
+  };
+
+  const handleEditClick = (vacancy: Vacancy) => {
+    setSelectedVacancy(vacancy);
+    setMode("edit");
+    setActiveButton(null);
+  };
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <section>
+          <header className="page-header">
+            <div className="page-header__container">
+              <div className="vac-link">
+                <button
+                  onClick={handleViewClick}
+                  className={`list-button ${
+                    activeButton === "view" ? "selected-button" : ""
+                  }`}
+                >
+                  Все заявки
+                </button>
+                <button
+                  onClick={handleAddClick}
+                  className={`new-button ${
+                    activeButton === "add" ? "selected-button" : ""
+                  }`}
+                >
+                  Создание заявки
+                </button>
+              </div>
+            </div>
+          </header>
+
+          <main className="page-main">
+            <div className="page-body__container">
+              {mode === "view" && (
+                <VacForm vacancies={vacancies} onEditClick={handleEditClick} />
+              )}
+              {mode === "add" && (
+                <VacAddView
+                  initialValues={vacancies}
+                  onSubmit={handleAddVacancy}
+                  onSuccess={() => setMode("view")}
+                />
+              )}
+              {mode === "edit" && (
+                <VacEditView
+                  initialValues={{
+                    VacName: selectedVacancy?.VacName || "",
+                    Ot: selectedVacancy?.Ot || "",
+                    salaryFrom: selectedVacancy?.salaryFrom || "",
+                    salary: selectedVacancy?.salary || "",
+                    salaryTo: selectedVacancy?.salaryTo || "",
+                    address: selectedVacancy?.address || "",
+                    underground: selectedVacancy?.underground || "",
+                    date: selectedVacancy?.date || "",
+                    PlanDate: selectedVacancy?.PlanDate || "",
+                    gender: selectedVacancy?.gender || "",
+                    experience: selectedVacancy?.experience || "",
+                    gr: selectedVacancy?.gr || "",
+                    employment_type: selectedVacancy?.employment_type || "",
+                  }}
+                  onUpdateVacancy={handleUpdateVacancy}
+                  onCancel={() => setMode("view")}
+                  setMode={setMode}
+                />
+              )}
+            </div>
+          </main>
+        </section>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
---
+++ src/const/const.ts
@@ -0,0 +1,6 @@
+export enum AppRoute {
+  Main ='/',
+  Vacancy = '/vacancy/:id',
+  VacancyEdit = '/vacancy-edit/:id',
+}
+
