3551b0be124be9ef01de2516bfcf074614b7c815
TromiUss <thepeaceofmind@yandex.ru>
2025-07-04 00:47:52 +0300

    optmz 1

--- src/view/vac-edit-view.tsx
+++ src/view/vac-edit-view.tsx
@@ -1,179 +1,19 @@
 import React from "react";
-import { Formik, Form, Field, ErrorMessage } from "formik";
-import * as Yup from "yup";
-import { Button, DatePicker } from "antd";
-import dayjs from "dayjs";
-import { VacEditViewProps } from "../types/vac-form.types";
-
-const validationSchema = Yup.object({
-  VacName: Yup.string().required('Обязательное поле'),
-  Ot: Yup.string().required('Обязательное поле'),
-  date: Yup.date().required('Обязательное поле'),
-  PlanDate: Yup.date().required('Обязательное поле'),
-  gender: Yup.string().required('Обязательное поле'),
-  address: Yup.string().required('Обязательное поле'),
-  skills: Yup.string().required('Обязательное поле'),
-  gr: Yup.string().required('Обязательное поле'),
-  employment_type: Yup.string().required('Обязательное поле')
-});
+import VacancyForm from "../components/vacancyForm";
 
 const VacEditView: React.FC<VacEditViewProps> = ({
   initialValues,
   onUpdateVacancy,
   onCancel,
-  setMode,
 }) => {
-  const initialVacancy = initialValues;
-  const initialFormValues = {
-    VacName: initialVacancy.VacName ?? "",
-    Ot: initialVacancy.Ot ?? "",
-    salaryTo: initialVacancy.salaryTo ?? "",
-    salaryFrom: initialVacancy.salaryFrom ?? "",
-    salary: initialVacancy.salary ?? "",
-    address: initialVacancy.address ?? "",
-    underground: initialVacancy.underground ?? "",
-    date: initialVacancy.date ? dayjs(initialVacancy.date) : null,
-    PlanDate: initialVacancy.PlanDate ? dayjs(initialVacancy.PlanDate) : null,
-    gender: initialVacancy.gender ?? "",
-    experience: initialVacancy.experience ?? "",
-    gr: initialVacancy.gr ?? "",
-    employment_type: initialVacancy.employment_type ?? "",
-  }
   return (
-    <label>
-          <h1>Форма размещения заявки</h1>
-          <Formik
-            enableReinitialize
-            initialValues={initialFormValues}
-            validationSchema={validationSchema}
-            onSubmit={(values, { setSubmitting }) => {
-              const parsedValues = {
-                ...values,
-                date: values.date ? values.date.toISOString() : "",
-                PlanDate: values.PlanDate ? values.PlanDate.toISOString() : "",
-                id: Date.now(),
-                publicationDate: new Date().toISOString(),
-              };
-              onUpdateVacancy(parsedValues.id, parsedValues);
-              setSubmitting(false);
-              setMode;
-              onCancel();
-            }}
-          >
-        {({ values, touched, setFieldValue, setTouched }) => (
-          <Form id="vacancyForm" className="add-vac__form">
-            <fieldset className="form-row-group">
-              <label>Наименование вакансии
-                <Field type="text" name="VacName" />
-                <ErrorMessage name="VacName" component="div" />
-              </label>
-
-              <label>Отдел
-                <Field type="text" name="Ot" />
-                <ErrorMessage name="Ot" component="div" />
-              </label>
-            </fieldset>
-            <fieldset className="form-row-group">
-              <label>Дата открытия
-                <DatePicker
-                  name="date"
-                  value={values.date ? dayjs(values.date) : null} 
-                  onChange={(date) => setFieldValue("date", date ? date.toISOString() : "")}
-                  onBlur={() => setTouched({ ...touched, date: true })}
-                />
-                <ErrorMessage name="date" component="div" />
-              </label>
-
-              <label>Плановая дата закрытия
-                <DatePicker
-                  name="PlanDate"
-                  value={values.PlanDate ? dayjs(values.PlanDate) : null}
-                  onChange={(date) => setFieldValue("PlanDate", date ? date.toISOString() : "")}
-                  onBlur={() => setTouched({ ...touched, PlanDate: true })}
-                />
-                <ErrorMessage name="PlanDate" component="div" />
-              </label>
-            </fieldset>
-            <fieldset>
-              <legend>Пол</legend>
-              <label className="radio-item">
-                <Field type="radio" name="gender" value="male" />
-                <span>Мужской</span>
-              </label>
-              <label className="radio-item">
-                <Field type="radio" name="gender" value="female" />
-                <span>Женский</span>
-              </label>
-              <ErrorMessage name="gender" component="div" />
-            </fieldset>
-            <fieldset className="form-row-group-salary">
-              <legend>Зарплата</legend>
-              <div className="onSide">
-                <label className="radio-item">
-                  <Field type="radio" name="salary" value="На руки" />
-                  <span>На руки</span>
-                </label>
-                <label className="radio-item">
-                  <Field type="radio" name="salary" value="До вычета налога" />
-                  <span>До вычета налога</span>
-                </label>
-              </div>
-              <div className="underSide">
-                <label className="input-group">
-                  От
-                  <Field type="text" name="salaryFrom" />
-                </label>
-                <label className="input-group">До
-                  <Field type="text" name="salaryTo" />
-                </label>
-              </div>
-            </fieldset>
-            <fieldset className="form-row-group">
-              <label>Адрес
-                <Field type="text" name="address" />
-                <ErrorMessage name="address" component="div" />
-              </label>
-
-              <label>Станция метро, МЦД
-                <Field type="text" name="underground" />
-              </label>
-            </fieldset>
-            <fieldset className="form-row-group">
-              <label>Опыт работы
-                <Field type="text" name="experience" />
-                <ErrorMessage name="experience" component="div" />
-              </label>
-
-              <label>График работы
-                <Field type="text" name="gr" />
-                <ErrorMessage name="gr" component="div" />
-              </label>
-            </fieldset>
-            <fieldset className="form-row-group">
-              <legend>Тип занятости</legend>
-              <label className="radio-item">
-                <Field type="radio" name="employment_type" value="full_time" />
-                <span>Полная занятость</span>
-              </label>
-              <label className="radio-item">
-                <Field type="radio" name="employment_type" value="part_time" />
-                <span>Частичная занятость</span>
-              </label>
-              <label className="radio-item">
-                <Field type="radio" name="employment_type" value="internship" />
-                <span>Стажировка</span>
-              </label>
-              <ErrorMessage name="employment_type" component="div" />
-            </fieldset>
-          </Form>
-        )}
-      </Formik>
-      <Button type="primary" form="vacancyForm" onClick={() => setMode("view")}>Сохранить</Button>
-      <Button type="primary" onClick={() => onCancel?.()}>Отменить</Button>
-    </label>
+    <VacancyForm
+      mode="edit"
+      initialValues={initialValues}
+      onSubmit={onUpdateVacancy}
+      onCancel={onCancel}
+    />
   );
 };
 
-
 export default VacEditView;
-
